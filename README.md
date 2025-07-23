ca11528a7b3a9cbda0fcba56603efd38f42846e1
chengxufeng <chengxufeng@bytedance.com>
2025-07-23 14:31:37 +0800

    feat: run with fastapi

---
+++ README.md
@@ -0,0 +1,40 @@
+# AI学习小项目
+
+## 环境安装
+
+- python3.13.3
+- uv (python的包管理工具，感觉比pip还有venv更好用)
+    ```
+    uv install
+    uv init
+    uv add xxx
+    ```
+- langchain (langchan相关的包一大堆)
+    ```
+    uv add langchain
+    ```
+
+
+# 做些小任务
+
+## 场景一 接入一个 chatmodel
+
+## 场景二 接入一个 vectorstore
+
+## 场景三 接入一个 retriever
+
+## 场景四 接入一个 llm
+
+## 场景五 接入一个 agent
+
+## 场景六 接入一个 tool
+
+## 场景七 接入一个 memory
+
+## 场景八 接入一个 embedding
+
+## 场景九 接入一个 向量数据库
+
+## 场景十 接入一个 文档数据库
+
+## 场景十一 接入微信读书mcp
--- first_lang.py
+++ first_lang.py
@@ -1,15 +1,55 @@
 from dotenv import load_dotenv
 import os
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage
+
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
 
 load_dotenv()
-
 OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-from langchain.chat_models import init_chat_model
+print(OPENAI_BASE_URL)
+print(OPENAI_API_KEY)
+# model = init_chat_model("gpt-4.1-mini", model_provider="openai")
 
-model = init_chat_model("gpt-4.1-mini", model_provider="openai")
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
 
-from langchain_core.messages import HumanMessage, SystemMessage
+# 2. Create model
+model = ChatOpenAI()
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+# 为根路径添加一个 GET 端点
+@app.get("/")
+async def redirect_root_to_docs():
+    return {"message": "Welcome to the translation server! Visit /docs for API documentation."}
+
+# 5. Adding chain route
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
 
 
 def main():
@@ -22,5 +62,7 @@ def main():
     
 
 if __name__ == "__main__":
-    main()
+    # main()
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8700)
     
